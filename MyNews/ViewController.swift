a482823e9e7070a3205b416c9aad415fc7675d8a
yurim <bigtoy2645@naver.com>
2020-10-14 03:06:00 +0900

    feat: project initial commit

---
+++ MyNews/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyNews
+//
+//  Created by yurim on 2020/10/07.
+//  Copyright © 2020 yurim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
