31cea99a7da53689be6f67c182f722c81156fbe6
yurim <bigtoy2645@naver.com>
2020-11-09 23:52:21 +0900

    feat: add RSSTableViewCell

---
+++ DevFeed/RSSTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  RSSTableViewCell.swift
+//  DevFeed
+//
+//  Created by yurim on 2020/11/09.
+//  Copyright © 2020 yurim. All rights reserved.
+//
+
+import UIKit
+
+class RSSTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var favicon: UIImageView!
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var link: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
