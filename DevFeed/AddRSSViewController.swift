4f732c8c1729e4316e4833eb959e8cde8d1af251
yurim <bigtoy2645@naver.com>
2020-11-26 19:49:43 +0900

    feat: add search function to AddRSSView

--- DevFeed/AddRSSViewController.swift
+++ DevFeed/AddRSSViewController.swift
@@ -12,7 +12,11 @@ class AddRSSViewController: UIViewController, UITableViewDelegate, UITableViewDa
     
     @IBOutlet weak var tblRSS: UITableView!
     
+    var searchController = UISearchController()
+    var resultVC = UITableViewController()
+    
     var rssList: [RSS] = []
+    var filteredRSSList: [RSS] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,28 +24,19 @@ class AddRSSViewController: UIViewController, UITableViewDelegate, UITableViewDa
         // Cell 등록
         let nibName = UINib(nibName: "RSSTableViewCell", bundle: nil)
         tblRSS.register(nibName, forCellReuseIdentifier: "RSSCell")
-    }
-    
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return rssList.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "RSSCell", for: indexPath) as! RSSTableViewCell
-//                cell.updateValue(feed: feed[indexPath.row])
+        resultVC.tableView.register(nibName, forCellReuseIdentifier: "RSSCell")
         
-        // favicon 다운로드
-        // TODO - protocol로 빼기
-        let url = URL(string: "https://www.google.com/s2/favicons?sz=64&domain=" + "\(rssList[indexPath.row].link)")
-        if let data = try? Data(contentsOf: url!) {
-            cell.favicon.image = UIImage(data: data)
-        }
-        cell.title.text = rssList[indexPath.row].name
-        cell.link.text = rssList[indexPath.row].link
-        cell.addButton.addTarget(self, action: #selector(addButtonDidTap(_:)), for: .touchUpInside)
+        // 검색 화면, 결과 화면 연결
+        searchController = UISearchController(searchResultsController: resultVC)
+        tblRSS.tableHeaderView = searchController.searchBar
         
-        return cell
+        self.definesPresentationContext = true
+        searchController.searchResultsUpdater = self
+        searchController.searchBar.delegate = self
+        searchController.delegate = self
+        
+        resultVC.tableView.delegate = self
+        resultVC.tableView.dataSource = self
     }
     
     /* 추가 버튼 선택 시 동작 */
@@ -58,5 +53,47 @@ class AddRSSViewController: UIViewController, UITableViewDelegate, UITableViewDa
 //        }
 //
 //        tblTodo.reloadData()
+    }
+    
+    // MARK: - UITableViewDelegate
+    
+    /* cell 개수 */
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tableView == resultVC.tableView ? filteredRSSList.count : rssList.count
+    }
+    
+    /* cell 그리기 */
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "RSSCell", for: indexPath) as! RSSTableViewCell
+        let rss: RSS = (tableView == resultVC.tableView) ? filteredRSSList[indexPath.row] : rssList[indexPath.row]
+        
+//                cell.updateValue(feed: feed[indexPath.row])
+        
+        // favicon 다운로드
+        // TODO - protocol로 빼기
+        let url = URL(string: "https://www.google.com/s2/favicons?sz=64&domain=" + "\(rss.link)")
+        if let data = try? Data(contentsOf: url!) {
+            cell.favicon.image = UIImage(data: data)
+        }
+        let rssLink = rss.link
+        cell.title.text = rss.name
+        cell.link.text = rssLink.replacingOccurrences(of: "https://", with: "")
+        cell.addButton.addTarget(self, action: #selector(addButtonDidTap(_:)), for: .touchUpInside)
+        
+        return cell
+    }
+}
+
+// MARK: - UISearch...
+
+extension AddRSSViewController: UISearchControllerDelegate, UISearchResultsUpdating, UISearchBarDelegate {
+    
+    /* 검색 키워드 변경 시 결과 화면 업데이트 */
+    func updateSearchResults(for searchController: UISearchController) {
+        filteredRSSList = rssList.filter({ (rss: RSS) -> Bool in
+            return rss.name.lowercased().contains(searchController.searchBar.text!.lowercased())
+        })
+        
+        resultVC.tableView.reloadData()
     }
 }
